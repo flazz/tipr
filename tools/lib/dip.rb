63dcfd0d220ca41429f0bc4b7cc78a316790e0d3
marly <marlified@gmail.com>
2009-05-13 15:03:06 -0400

    factoring out some shared functionality for processing descriptors

--- tools/lib/dip.rb
+++ tools/lib/dip.rb
@@ -10,11 +10,13 @@ require 'representation'
 require 'validatable'
 require 'digest/sha1'
 require 'valid'
+require 'tiprhelpers'
 
 
 class DIP
   include Validatable
   include Validity
+  include TIPRHelpers
 
   # The AIP should point to actual files
   validates_true_for :completeness, :logic => lambda {files_exist?}
@@ -30,11 +32,6 @@ class DIP
   attr_reader :ieid, :package_id, :create_date, :original_representation, 
               :current_representation, :migration_map
   
-  NS = {
-    'mets' => 'http://www.loc.gov/METS/',
-    'daitss' => 'http://www.fcla.edu/dls/md/daitss/',
-    'xlink' => 'http://www.w3.org/1999/xlink'
-  }
 
   def initialize(path)
     @path = path.chomp('/')     # Strip any trailing /es
@@ -42,11 +39,11 @@ class DIP
     @global_descriptor_path = load_descriptor_path(true)
     @doc = load_descriptor
     @global_doc = load_descriptor(true)
-    @ieid = load_ieid
+    @ieid = load_ieid(@doc)
     @package_id = load_package_id
     @create_date = load_create_date
-    @dfid_map  = load_dfid_map
-    @global_dfid_map = load_dfid_map(true)
+    @dfid_map  = load_dfid_map(@doc)
+    @global_dfid_map = load_dfid_map(@global_doc) unless @global_doc.nil?
     @migration_map = load_migration_map
     @submitting_agent = load_submitting_agent
     @original_representation = load_original_representation
@@ -56,17 +53,7 @@ class DIP
   def global_files?
     @global_doc ? true : false
   end
-  
-  # Given an OID, retrieve events
-  def events(oid, global=false)
-    doc = global ? @global_doc : @doc
-    return [] unless doc
     
-    doc.xpath('//daitss:EVENT', NS).select do |event|
-      event.xpath('./daitss:OID', NS).first.content == oid
-    end
-  end
-  
   def package_path
     package = @path.split("/").last
     @path.split(package).first
@@ -101,13 +88,7 @@ class DIP
     descriptor = global ? @global_descriptor_path : @descriptor_path
     open(descriptor) { |io| Nokogiri::XML io } if descriptor
   end
-  
-  def load_ieid
-    ieid_node = @doc.xpath('//daitss:IEID', NS).first
-    raise "IEID not found" unless ieid_node
-    ieid_node.content
-  end
-  
+    
   def load_package_id
     package_id_node = @doc.xpath('//daitss:ENTITY_ID', NS).first
     raise "PACAKGE ID not found" unless package_id_node
@@ -120,25 +101,7 @@ class DIP
     Time.parse create_date_node.content
   end
   
-  
-  # Create a map between DFIDs and structMap FILEIDs
-  def load_dfid_map(global=false)
-
-    doc = global ? @global_doc : @doc
-    return {} if doc.nil?       # Nothing to map
-
-    file_ids = doc.xpath('//mets:file/@ID', NS).map { |a| a.value }
-    dfid_map = Hash.new
-    
-    file_ids.compact.each do |fid|
-      tmd = doc.xpath("//mets:file[@ID = '#{fid}']", NS).first['ADMID']
-      dfid = doc.xpath("//mets:techMD[@ID = '#{tmd}']//daitss:DFID", NS).first.content
-      dfid_map[:"#{fid}"] = dfid 
-    end
-    dfid_map
-  end
- 
-  
+   
   # Create a map between migrated FILEIDs
   def load_migration_map
 
@@ -174,13 +137,6 @@ class DIP
     { :name => name, :project_code => project_code, :type => "organization" }
   end
   
-  
-  # retrieve the DAITSS:FORMAT of the file from the given TechMD ID
-  def file_format(tmd_id, global=false)
-    doc = global ? @global_doc : @doc    
-    doc.xpath("//mets:techMD[@ID='#{tmd_id}']//daitss:FORMAT", NS).first.content
-  end
-
   # Load our representation  
   def load_representation(type, global=false, rep=nil)
 
---
+++ tools/lib/tiprhelpers.rb
@@ -0,0 +1,67 @@
+require 'nokogiri'
+
+# Some shared functionality when generating a TIPR DIP from the AIP and GFPs
+module TIPRHelpers
+
+  NS = {
+    'mets' => 'http://www.loc.gov/METS/',
+    'daitss' => 'http://www.fcla.edu/dls/md/daitss/',
+    'xlink' => 'http://www.w3.org/1999/xlink'
+  }
+
+  # Load the ieid of a package
+  def load_ieid(descriptor)
+    ieid_node = descriptor.xpath('//daitss:IEID', NS).first
+    raise "IEID not found" unless ieid_node
+    ieid_node.content
+  end
+
+  # Create a FID --> DFID hash from a parsed DAITSS METS descriptor
+  def load_dfid_map(descriptor)
+
+    file_ids = descriptor.xpath('//mets:file/@ID', NS).map { |a| a.value }
+    dfid_map = Hash.new
+    
+    file_ids.compact.each do |fid|
+      tmd = descriptor.xpath("//mets:file[@ID = '#{fid}']", NS).first['ADMID']
+      dfid = descriptor.xpath("//mets:techMD[@ID = '#{tmd}']//daitss:DFID", 
+                             NS).first.content
+      dfid_map[:"#{fid}"] = dfid 
+    end
+
+    dfid_map
+
+  end
+  
+  # Retrieve events from a descriptor
+  def events(oid, descriptor)
+    descriptor.xpath('//daitss:EVENT', NS).select do |event|
+      event.xpath('./daitss:OID', NS).first.content == oid
+    end    
+  end
+
+  # Retrieve events by REL_OID from a descriptor
+  def related_events(reloid, descriptor)
+    descriptor.xpath('//daitss:EVENT', NS).select do |event|
+      event.xpath('./daitss:REL_OID', NS).first.content == reloid
+    end
+  end
+
+  # Retrieve the DAITSS:FORMAT of the file from the given TechMD ID
+  def file_format(tmd_id, descriptor)
+    node_path = "//mets:techMD[@ID='#{tmd_id}']//daitss:FORMAT"
+    descriptor.xpath(node_path, NS).first.content
+  end
+
+  # Return the checksum, file path, oid, and events
+  def file_info(file_node, path, dmap, descriptor)
+    file = file_node.xpath('mets:FLocat/@xlink:href', NS).first.content
+    full_file_path = File.join(path, file) 
+    oid = dmap[:"#{file_node['ID']}"]
+    format = file_format(file_node['ADMID'], descriptor)
+    
+    { :sum => file_node['CHECKSUM'], :oid => oid, :path => full_file_path,
+      :format => format }
+  end
+
+end
